a54cd22cfb5a3c860bf10ea8af333c421a965c0a
jisoooit <ydsn336@naver.com>
2023-01-26 16:17:53 +0900

    fix : 파일 수정 실험

--- src/main/java/com/example/deploypractice/controller/TestController.java
+++ src/main/java/com/example/deploypractice/controller/TestController.java
@@ -11,11 +11,11 @@ public class TestController {
 
     @GetMapping("/deploy/test")
     public String hello(@RequestParam(defaultValue = "1") Integer number){
-        if(number == 1){ // info 로그
+        if(number == 1){
             log.info("/deploy/test 이 호출되었어요. info 로그 #####################################");
-        }else if(number == -1){ // error 로그
+        }else if(number == -1){
             log.error("/deploy/test 이 호출되었어요. error 로그 #####################################");
-        }else if(number == 0){ // warn 로그
+        }else if(number == 0){
             log.warn("/deploy/test 이 호출되었어요. warn 로그 #####################################");
         }
 
